d142c3fbc8906ef9b26cff1901f281161c61f8af
xiaofei <lixiaofei0627@gmail.com>
2023-05-31 17:33:51 +0800

    docs(docs): :memo: edit hmt.js.

--- docs/public/hmt.js
+++ docs/public/hmt.js
@@ -1,9 +1,8 @@
-<script>
-var _hmt = _hmt || [];
+ var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?3424c5819a5d8516b46e368d17653988";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-</script>
+ 
