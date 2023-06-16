5a781ce9bc0c25204c5883b818bbe92710a9b90c
guosong <879171160@qq.com>
2023-06-16 11:21:48 +0800

    feat(components): :sparkles: add radio and date-picker component.

--- packages/play/src/__nav.vue
+++ packages/play/src/__nav.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <router-link class="link" to="/dialog">dialog</router-link>
+    <router-link class="link" to="/date-picker">date-picker</router-link>
     <router-link class="link" to="/image-preview">image-preview</router-link>
     <router-link class="link" to="/pagination">pagination</router-link>
+    <router-link class="link" to="/radio">radio</router-link>
     <router-link class="link" to="/table">table</router-link>
   </div>
 </template>
---
+++ packages/play/src/views/radio.vue
@@ -0,0 +1,26 @@
+<template>
+  <PlusRadio v-model="state.value" :data="state.data" @change="change" />
+</template>
+
+<script setup lang="ts">
+import { reactive, watch } from 'vue'
+
+const state = reactive({
+  data: [
+    { label: 'Option A', value: 1 },
+    { label: 'Option B', value: 2 }
+  ],
+  value: 1
+})
+
+watch(
+  () => state.value,
+  val => {
+    console.log(val)
+  },
+  { immediate: true }
+)
+const change = () => {
+  console.log(3333)
+}
+</script>
