3316496c0d41263c025ccba0a57e4d7e7321ea70
xiaofei <lixiaofei0627@gmail.com>
2023-08-21 14:54:12 +0800

    docs(docs): :memo: fix docs-tip component with 'ClientOnly'

--- docs/.vitepress/theme/components/docs-tip.vue
+++ docs/.vitepress/theme/components/docs-tip.vue
@@ -1,12 +1,14 @@
 <template>
-  <el-tooltip raw-content effect="light" trigger="click">
-    <template #content>
-      <code class="docs-tip-code">
-        {{ content }}
-      </code>
-    </template>
-    <el-icon :size="16" class="docs-tip-icon"><Warning /></el-icon>
-  </el-tooltip>
+  <ClientOnly>
+    <el-tooltip raw-content effect="light" trigger="click">
+      <template #content>
+        <code class="docs-tip-code">
+          {{ content }}
+        </code>
+      </template>
+      <el-icon :size="16" class="docs-tip-icon"><Warning /></el-icon>
+    </el-tooltip>
+  </ClientOnly>
 </template>
 
 <script lang="ts" setup>
