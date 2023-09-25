a380dc845e0da820f9234f3042af8ca934fad9a7
guosong <879171160@qq.com>
2023-09-25 11:09:31 +0800

    feat(components[input-tag]): :sparkles: add input-tag component

---
+++ packages/components/input-tag/style/css.ts
@@ -0,0 +1,4 @@
+import '@external/input/style/css'
+import '@external/tag/style/css'
+
+import '@plus-pro-components/theme-chalk/plus-input-tag.css'
---
+++ packages/components/input-tag/style/index.ts
@@ -0,0 +1,4 @@
+import '@external/input/style/index'
+import '@external/tag/style/index'
+
+import '@plus-pro-components/theme-chalk/src/input-tag.scss'
