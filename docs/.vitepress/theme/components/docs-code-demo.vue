cc50a8ef389bde17130eb43214317fb40cb430d6
xiaofei <lixiaofei0627@gmail.com>
2023-10-18 15:25:56 +0800

    docs(docs): :memo: docs add Playground edit and GitHub edit.

--- docs/.vitepress/theme/components/docs-code-demo.vue
+++ docs/.vitepress/theme/components/docs-code-demo.vue
@@ -4,11 +4,29 @@
       <AppAsyncComponent />
     </div>
     <div class="docs-example-btns">
-      <el-tooltip content="复制代码">
-        <el-icon :size="18"><DocumentCopy v-copy="content" /></el-icon>
+      <el-tooltip content="在 Playground 中编辑">
+        <el-icon :size="size" @click="handleEditPlayground">
+          <svg
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+            width="1.2em"
+            height="1.2em"
+            data-v-5d9e4641=""
+          >
+            <path
+              d="M16 2v2h-1v3.243c0 1.158.251 2.301.736 3.352l4.282 9.276A1.5 1.5 0 0 1 18.656 22H5.344a1.5 1.5 0 0 1-1.362-2.129l4.282-9.276A7.994 7.994 0 0 0 9 7.243V4H8V2h8zm-2.612 8.001h-2.776c-.104.363-.23.721-.374 1.071l-.158.361L6.125 20h11.749l-3.954-8.567a9.978 9.978 0 0 1-.532-1.432zM11 7.243c0 .253-.01.506-.029.758h2.058a8.777 8.777 0 0 1-.021-.364L13 7.243V4h-2v3.243z"
+            ></path>
+          </svg>
+        </el-icon>
       </el-tooltip>
-      <el-tooltip :content="show ? '收起' : '展开'">
-        <el-icon :size="18" @click="handleToggle">
+      <el-tooltip content="在 GitHub 中编辑">
+        <el-icon :size="size" @click="handleEdit"><EditPen /></el-icon>
+      </el-tooltip>
+      <el-tooltip content="复制代码">
+        <el-icon :size="size"><DocumentCopy v-copy="content" /></el-icon>
+      </el-tooltip>
+      <el-tooltip :content="show ? '收起源代码' : '查看源代码'">
+        <el-icon :size="size" @click="handleToggle">
           <ArrowUp v-if="show" />
           <ArrowDown v-else />
         </el-icon>
@@ -35,8 +53,14 @@
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-import { DocumentCopy, ArrowDown, ArrowUp } from '@element-plus/icons-vue'
+import { DocumentCopy, ArrowDown, ArrowUp, EditPen, Position } from '@element-plus/icons-vue'
 import { getComponent } from '../../utils/getComponent'
+import { getPlaygroundEncoded } from '../../utils'
+
+// 编辑地址
+const editUrl = 'https://github.com/plus-pro-components/plus-pro-components/edit/dev/docs/examples'
+const editPlaygroundUrl = 'https://playground.plus-pro-components.com'
+// const editPlaygroundUrl = 'http://127.0.0.1:3002'
 
 // 自动加载 asyncRouter 文件夹下所有的异步路由
 const moduleFiles = import.meta.glob('../../../examples/**/*.vue', {
@@ -51,6 +75,7 @@ const props = defineProps<{
 
 const show = ref(false)
 const total = ref(0)
+const size = 18
 
 const AppAsyncComponent = getComponent(moduleFiles, props.path)
 
@@ -64,6 +89,19 @@ const content = computed(() => decodeURIComponent(props.rawSource))
 // add line-number
 const tem = content.value.split('\r\n')
 total.value = tem.length
+
+// 去github编辑
+const handleEdit = () => {
+  const url = `${editUrl}/${props.path}.vue`
+  window.open(url, '_blank')
+}
+
+// 去Playground编辑
+const handleEditPlayground = () => {
+  const encoded = getPlaygroundEncoded(props.rawSource)
+  const url = `${editPlaygroundUrl}/#${encoded}`
+  window.open(url, '_blank')
+}
 </script>
 
 <style lang="scss" scoped>
@@ -85,6 +123,7 @@ total.value = tem.length
   .el-icon {
     cursor: pointer;
     margin-left: 20px;
+    color: #747578;
   }
 }
 .content {
