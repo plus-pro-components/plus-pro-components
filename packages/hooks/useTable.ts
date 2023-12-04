3fd40c97f8131ce2e5757dbc00bfc1783587edab
xiaofei <lixiaofei0627@gmail.com>
2023-12-04 13:44:16 +0800

    feat(hooks): :sparkles: useTable add 'pageInfo' params.

--- packages/hooks/useTable.ts
+++ packages/hooks/useTable.ts
@@ -1,4 +1,5 @@
-import { ref, shallowRef } from 'vue'
+import type { Ref } from 'vue'
+import { ref, shallowRef, unref } from 'vue'
 import { DefaultPageInfo } from '@plus-pro-components/constants'
 import type { ActionBarButtonsRow } from '@plus-pro-components/components/table'
 import type { PageInfo } from '@plus-pro-components/types'
@@ -7,9 +8,12 @@ import type { PageInfo } from '@plus-pro-components/types'
  * 初始化表格基本数据
  *
  */
-export function useTable<T extends Record<string, any>[] = any>() {
+export function useTable<T extends Record<string, any>[] = any>(
+  _pageInfo: PageInfo | Ref<PageInfo>
+) {
+  const defaultPageInfo = unref(_pageInfo) || DefaultPageInfo
   const tableData = ref<T>([] as any)
-  const pageInfo = ref<PageInfo>({ ...DefaultPageInfo })
+  const pageInfo = ref<PageInfo>({ ...defaultPageInfo })
   const total = ref<number>(0)
   const loadingStatus = ref<boolean>(false)
   const buttons = shallowRef<ActionBarButtonsRow[]>([])
