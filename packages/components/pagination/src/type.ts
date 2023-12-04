b35a298252f159c3f782dd6e78aa4e18fa471e23
xiaofei <lixiaofei0627@gmail.com>
2023-12-04 13:47:11 +0800

    fix(components): :bug: fix  pagination component 'PlusPaginationProps' typescript type.

--- packages/components/pagination/src/type.ts
+++ packages/components/pagination/src/type.ts
@@ -2,7 +2,6 @@ import type { PageInfo } from '@plus-pro-components/types'
 
 export interface PlusPaginationProps {
   total?: number
-  show?: boolean
   pageSizeList?: number[]
   modelValue?: PageInfo
 }
