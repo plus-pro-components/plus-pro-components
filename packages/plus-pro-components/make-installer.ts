a5098f9a468d9a38b0b919c27724fced21ee37a6
xiaofei <lixiaofei0627@gmail.com>
2024-02-18 13:07:04 +0800

    feat(plus-pro-components): :sparkles: update install type.

--- packages/plus-pro-components/make-installer.ts
+++ packages/plus-pro-components/make-installer.ts
@@ -1,7 +1,7 @@
 import type { App, Component } from 'vue'
 
 export const makeInstaller = (components: Component[] = []) => {
-  const install = (app: App): any => {
+  const install = (app: App) => {
     components.forEach((component: Component) => app.component(component.name as string, component))
   }
   return {
