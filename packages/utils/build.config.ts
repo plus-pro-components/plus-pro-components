7f6f21108a020c365de1dc09d00b300dc4ea060a
xiaofei <lixiaofei0627@gmail.com>
2024-02-18 13:10:05 +0800

    feat(utils): :sparkles: update utils build method.

--- packages/utils/build.config.ts
+++ packages/utils/build.config.ts
@@ -1,7 +1,14 @@
+import path from 'node:path'
 import { defineBuildConfig } from 'unbuild'
+import glob from 'fast-glob'
+
+const entries = glob.sync('./src/*', {
+  cwd: path.resolve(__dirname),
+  onlyFiles: true
+})
 
 export default defineBuildConfig({
-  entries: ['./src/index'],
+  entries,
   clean: true,
   declaration: true,
   rollup: {
