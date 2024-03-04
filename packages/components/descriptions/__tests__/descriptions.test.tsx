2f2f079f1db5d897b7f0317c1110b0798014d185
xiaofei <lixiaofei0627@gmail.com>
2024-03-04 14:33:25 +0800

    test(components): :white_check_mark: descriptions component add test case

--- packages/components/descriptions/__tests__/descriptions.test.tsx
+++ packages/components/descriptions/__tests__/descriptions.test.tsx
@@ -5,82 +5,6 @@ import ElementPlus from 'element-plus'
 import type { PlusColumn } from '@plus-pro-components/types'
 import Descriptions from '../src/index.vue'
 
-const columns: PlusColumn[] = [
-  {
-    label: '名称',
-    width: 120,
-    prop: 'name',
-    valueType: 'copy'
-  },
-  {
-    label: '状态',
-    width: 120,
-    prop: 'status',
-    valueType: 'select',
-    options: [
-      {
-        label: '未解决',
-        value: '0',
-        color: 'red'
-      },
-      {
-        label: '已解决',
-        value: '1',
-        color: 'blue'
-      },
-      {
-        label: '解决中',
-        value: '2',
-        color: 'yellow'
-      },
-      {
-        label: '失败',
-        value: '3',
-        color: 'red'
-      }
-    ]
-  },
-  {
-    label: '标签',
-    width: 120,
-    prop: 'tag',
-    valueType: 'tag'
-  },
-  {
-    label: '执行进度',
-    width: 200,
-    prop: 'progress',
-    valueType: 'progress'
-  },
-  {
-    label: '代码块',
-    width: 250,
-    prop: 'code',
-    valueType: 'code'
-  },
-  {
-    label: '评分',
-    width: 200,
-    prop: 'rate',
-    valueType: 'rate'
-  },
-  {
-    label: '开关',
-    width: 100,
-    prop: 'switch',
-    valueType: 'switch',
-    editable: true
-  },
-  {
-    label: '时间',
-    width: 190,
-    prop: 'time',
-    valueType: 'date-picker'
-  }
-]
-
-const labels = columns.map(item => item.label)
-
 const TestServe = {
   getList: async () => {
     const index = Math.random() * 10
@@ -117,6 +41,81 @@ const TestServe = {
 
 describe('descriptions/index.vue', () => {
   test('render test', async () => {
+    const columns: PlusColumn[] = [
+      {
+        label: '名称',
+        width: 120,
+        prop: 'name',
+        valueType: 'copy'
+      },
+      {
+        label: '状态',
+        width: 120,
+        prop: 'status',
+        valueType: 'select',
+        options: [
+          {
+            label: '未解决',
+            value: '0',
+            color: 'red'
+          },
+          {
+            label: '已解决',
+            value: '1',
+            color: 'blue'
+          },
+          {
+            label: '解决中',
+            value: '2',
+            color: 'yellow'
+          },
+          {
+            label: '失败',
+            value: '3',
+            color: 'red'
+          }
+        ]
+      },
+      {
+        label: '标签',
+        width: 120,
+        prop: 'tag',
+        valueType: 'tag'
+      },
+      {
+        label: '执行进度',
+        width: 200,
+        prop: 'progress',
+        valueType: 'progress'
+      },
+      {
+        label: '代码块',
+        width: 250,
+        prop: 'code',
+        valueType: 'code'
+      },
+      {
+        label: '评分',
+        width: 200,
+        prop: 'rate',
+        valueType: 'rate'
+      },
+      {
+        label: '开关',
+        width: 100,
+        prop: 'switch',
+        valueType: 'switch',
+        editable: true
+      },
+      {
+        label: '时间',
+        width: 190,
+        prop: 'time',
+        valueType: 'date-picker'
+      }
+    ]
+    const labels = columns.map(item => item.label)
+
     const descriptionsData = ref<any>({})
     const wrapper = mount(() => <Descriptions columns={columns} data={descriptionsData.value} />, {
       global: {
@@ -152,5 +151,68 @@ describe('descriptions/index.vue', () => {
     })
     await nextTick()
     expect(wrapper.find('.el-descriptions__title').text()).toContain(title)
+  })
+
+  test('slots test', async () => {
+    const columns: PlusColumn[] = [
+      {
+        label: '名称',
+        width: 120,
+        prop: 'name'
+      },
+      {
+        label: '状态',
+        width: 120,
+        prop: 'status',
+        valueType: 'select',
+        options: [
+          {
+            label: '未解决',
+            value: '0',
+            color: 'red'
+          },
+          {
+            label: '已解决',
+            value: '1',
+            color: 'blue'
+          },
+          {
+            label: '解决中',
+            value: '2',
+            color: 'yellow'
+          },
+          {
+            label: '失败',
+            value: '3',
+            color: 'red'
+          }
+        ]
+      }
+    ]
+    const title = 'title'
+    const data = {
+      index: 0,
+      status: '1',
+      name: '小明'
+    }
+    const slot = {
+      ['plus-desc-name']: ({ value }: { value: string }) => '自定义' + value,
+      ['plus-desc-label-name']: ({ label }: { label: string }) => '自定义label' + label,
+      ['plus-desc-status']: ({ value }: { value: string }) => '自定义' + value,
+      ['plus-desc-label-status']: ({ label }: { label: string }) => '自定义label' + label
+    }
+    const wrapper = mount(
+      () => <Descriptions title={title} columns={columns} data={data} v-slots={slot} />,
+      {
+        global: {
+          plugins: [ElementPlus]
+        }
+      }
+    )
+    await nextTick()
+    expect(wrapper.find('.plus-description').text()).toContain('自定义小明')
+    expect(wrapper.find('.plus-description').text()).toContain('自定义label名称')
+    expect(wrapper.find('.plus-description').text()).toContain('自定义1')
+    expect(wrapper.find('.plus-description').text()).toContain('自定义label状态')
   })
 })
