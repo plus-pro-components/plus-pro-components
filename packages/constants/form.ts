27171b81f963d7fc86e0b7810703242f60bd20f7
xiaofei <lixiaofei0627@gmail.com>
2024-03-18 10:51:30 +0800

    feat(constants): :sparkles: datePickerValueIsArrayList add 'years','dates'

--- packages/constants/form.ts
+++ packages/constants/form.ts
@@ -2,3 +2,30 @@
  * 表格里表单的provide(inject) key
  */
 export const TableFormRefInjectionKey = Symbol('tableFormRefInjectionKey')
+
+/**
+ *  表单ElDatePicker 组件 值是数组的type列表
+ */
+export const DatePickerValueIsArrayList = [
+  'datetimerange',
+  'daterange',
+  'monthrange',
+  'years',
+  'dates'
+]
+
+/**
+ * 表单组件值是数字类型的valueType列表
+ */
+export const ValueIsNumberList = ['rate', 'input-number', 'slider']
+
+/**
+ * 表单组件值是数字类型的valueType列表
+ */
+
+export const ValueIsBooleanList = ['switch']
+
+/**
+ *  表单组件值是数组的valueType列表
+ */
+export const ValueIsArrayList = ['checkbox', 'cascader', 'plus-date-picker', 'plus-input-tag']
