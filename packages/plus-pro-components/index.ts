19e5978d9b4f581487fbda240ab44450859543cc
xiaofei <lixiaofei0627@gmail.com>
2024-03-20 16:55:31 +0800

    feat(plus-pro-components): :sparkles: export version

--- packages/plus-pro-components/index.ts
+++ packages/plus-pro-components/index.ts
@@ -5,5 +5,7 @@ export * from '@plus-pro-components/hooks'
 export * from '@plus-pro-components/types'
 export * from '@plus-pro-components/constants'
 
+export { version } from './version'
+
 export const install = installer.install
 export default installer
