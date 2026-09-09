85f9d34c28effc95501474b9bc24f5110f1f3a4d
cocotun <1556237068@qq.com>
2026-09-09 16:10:55 +0800

    fix(input-tag): 修复长标签溢出布局问题并新增演示示例

--- packages/play/src/views/input-tag.vue
+++ packages/play/src/views/input-tag.vue
@@ -1,9 +1,11 @@
 <template>
   <PlusInputTag v-model="data" :limit="3" />
+  <PlusInputTag v-model="data1" style="max-width: 200px" />
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
 const data = ref(['tag1', 'tag2', 'tag11', 'tag22'])
+const data1 = ref([])
 </script>
